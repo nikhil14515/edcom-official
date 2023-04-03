6f99e8636ac46c57ee10d0c2274552ad4c2e6db8
Nikhil Ram Goud Buddiga <93340144+nikhil14515@users.noreply.github.com>
2023-04-03 19:41:55 +0530

    Changes by NEO
    Added share buttons

--- contest_indiv_js.js
+++ contest_indiv_js.js
@@ -10,3 +10,36 @@ function visible(){
       im.src='icons/menu-icon.png'
   }
 }
+
+var id="none"
+var copyText = document.querySelector(".contest-btn-white");
+function change(){
+  document.getElementById(id).innerHTML="SHARE";
+  document.getElementById(id).style.color = 'white'
+  document.getElementById(id).style.backgroundColor = '#001F54'
+}
+function copy(n) {
+  if (n==1){
+    var text="https://edcom-official.netlify.app/event_codehunt"
+    id="copy1"
+  }
+  else if(n==2){
+    var text="https://edcom-official.netlify.app/event_codeDebugging"
+    id="copy2"
+    
+  }
+  else if(n==3){
+    var text="https://edcom-official.netlify.app/event_appDev"
+    id="copy3"
+  }
+  navigator.clipboard.writeText(text)
+  document.getElementById(id).innerHTML="COPIED"
+  setTimeout(change,3500)
+    .then(() => {
+      console.log('Text copied to clipboard');
+     })
+    .catch((error) => {
+      console.error('Error copying text: ', error);
+    });
+    
+}
